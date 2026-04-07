c33e441ba421dd783e7bf5653f2635260d5bfb11
abhijith <abhijitht210@gmail.com>
2026-04-07 20:47:05 +0530

    contents

--- src/Pages/PostDetails.jsx
+++ src/Pages/PostDetails.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { getBlog } from '../api/devto';
+import { useParams } from 'react-router-dom';
 
 function PostDetails() {
+    const { id } = useParams()
+    const [blog, setBlog] = useState({})
+
+    useEffect(() => {
+        const fetchBlog = async () => {
+            try {
+                const data = await getBlog(id)
+                setBlog(data)
+            } catch (error) {
+                console.log(error);
+            }
+        }
+
+        fetchBlog()
+    }, [id])
+
     return (
-        <div>PostDetails</div>
+        <div className='px-6 py-7'>
+
+            <>
+                <div dangerouslySetInnerHTML={{ __html: blog.body_html }} />
+
+            </>
+        </div >
     )
 }
 
--- src/api/devto.js
+++ src/api/devto.js
@@ -4,3 +4,8 @@ export const getBlogs = async () => {
     const res = await axios.get("https://dev.to/api/articles?page=1&per_page=10")
     return res.data
 }
+
+export const getBlog = async (id) => {
+    const res = await axios.get(`https://dev.to/api/articles/${id}`)
+    return res.data
+}
